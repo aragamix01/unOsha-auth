808998951de608310e5e92f00a8a09188f3f7d87
THAMMARAT KERDLUMJIAK  (CDGS-ADD) <thammarat.k@cdg.co.th>
2020-07-26 22:04:50 +0700

    updated

--- src/resolvers.js
+++ src/resolvers.js
@@ -23,5 +23,23 @@ export default {
       });
       return await User.findOne({ username });
     },
+    async login(root, { input: { username, password } }) {
+      // const conditions = Boolean(username) && Boolean(password);
+      // if (conditions) {
+      //   throw new Error('username or password is invalid');
+      // }
+
+      const user = await User.findOne({ username });
+      if (!user) {
+        throw new Error('Email not found');
+      }
+
+      const validPassword = await bcrypt.compare(password, user.password);
+      if (!validPassword) {
+        throw new Error('Password is incorrect');
+      }
+
+      return user;
+    },
   },
 };
